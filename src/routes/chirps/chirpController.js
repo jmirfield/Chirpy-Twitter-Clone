dcde6d5c2403faac0cb4520bebe921c51237578b
Justin Mirfield <justin.mirfield@gmail.com>
2022-02-10 13:30:19 -0500

    Finished up rechirp function

--- src/routes/chirps/chirpController.js
+++ src/routes/chirps/chirpController.js
@@ -27,10 +27,6 @@ class ChirpController {
                     path: 'user'
                 }
             })
-            const retweetQuery = []
-            req.user.following.forEach(follow => {
-                retweetQuery.concat(follow.user.retweetedChirps)
-            })
             const followQuery = req.user.following.map(user => user.following_id)
             const chirps = await Chirp.find({
                 $or: [
@@ -62,11 +58,34 @@ class ChirpController {
             req.user.retweetedChirps.addToSet(chirp.rechirp.original_id)
             if (startingLength !== req.user.retweetedChirps.length) {
                 await req.user.save()
+                await chirp.save()
+                await Chirp.findOneAndUpdate({ _id: chirp.rechirp.original_id }, { $inc: { rechirpsCount: 1 } })
+                await Chirp.updateMany({ 'rechirp.original_id': chirp.rechirp.original_id }, { $inc: { rechirpsCount: 1 } })
             }
-            await chirp.save()
-            await Chirp.findOneAndUpdate({ _id: chirp.rechirp.original_id }, { $inc: { rechirpsCount: 1 } })
-            await Chirp.updateMany({ 'rechirp.original_id': chirp.rechirp.original_id }, { $inc: { rechirpsCount: 1 } })
-            res.send(chirp)
+            res.status(202).send(chirp)
+        } catch (e) {
+            console.log(e)
+            res.status(400).send()
+        }
+    }
+
+    deleteRechirp = async (req, res) => {
+        try {
+            if (!req.body._id) throw new Error('Cannot provide null value')
+            const startingLength = req.user.retweetedChirps.length
+            req.user.retweetedChirps.pull({ _id: req.body._id })
+            if (startingLength !== req.user.retweetedChirps.length) {
+                await req.user.save()
+                await Chirp.findOneAndDelete({
+                    $and: [
+                        { owner_username: req.user.username },
+                        { 'rechirp.original_id': req.body._id }
+                    ]
+                })
+                await Chirp.findOneAndUpdate({ _id: req.body._id }, { $inc: { rechirpsCount: -1 } })
+                await Chirp.updateMany({ 'rechirp.original_id': req.body._id }, { $inc: { rechirpsCount: -1 } })
+            }
+            res.status(202).send()
         } catch (e) {
             console.log(e)
             res.status(400).send()
--- src/routes/chirps/chirpRouter.js
+++ src/routes/chirps/chirpRouter.js
@@ -5,7 +5,7 @@ const auth = require('../../middleware/auth')
 
 router.post('/chirps', auth, controller.createChirp)
 router.post('/chirps/rechirp', auth, controller.addRechirp)
-// router.post('/chirps/rechirp', auth, controller.addRechirp)
+router.delete('/chirps/rechirp/delete', auth, controller.deleteRechirp)
 router.get('/chirps/feed', auth, controller.getCurrentUserChirpFeed)
 router.get('/chirps/auth/:username', auth, controller.getUserChirps)
 
