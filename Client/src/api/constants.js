dab232d36b6b60128d48d74c7eabdd5423084e20
jmirfield <justin.mirfield@gmail.com>
2022-02-25 14:09:45 -0500

    Fixed styling for smaller devices

--- Client/src/api/constants.js
+++ Client/src/api/constants.js
@@ -1,1 +1,2 @@
 export const API_URL = 'http://localhost:3001/';
+// export const API_URL = 'http://192.168.0.7:3001/';
--- Client/src/components/Profile/ProfileList.js
+++ Client/src/components/Profile/ProfileList.js
@@ -1,26 +1,66 @@
-import React from 'react'
+import React, { useReducer, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import ProfileImage from '../UI/ProfileImage/ProfileImage'
+import FollowButton from '../UI/FollowButton/FollowButton'
 import styles from './styles.module.css'
+import { followRequest, unfollowRequest } from '../../actions/profile'
+import AuthContext from '../../context/AuthContext'
+
+const initialState = {
+    isFollow: false
+}
+
+const followReducer = (state, action) => {
+    switch (action.type) {
+        case 'FOLLOW':
+            return { isFollow: true }
+        case 'UNFOLLOW':
+            return { isFollow: false }
+    }
+}
 
 const ProfileList = (props) => {
     return (
         <li className={styles.profile__list}>
-            {props.users.map((user, idx) => {
+            {props.users.map((user) => {
                 return (
-                    <ul key={idx} className={styles.profile__item}>
-                        <Link to={`/${user.username}`}>
-                            <ProfileImage
-                                className={styles.profile__user__icon}
-                                src={user.image}
-                            />
-                            <span className={styles.profile__user}>{user.username}</span>
-                        </Link>
-                    </ul>
+                    <ProfileMin
+                        key={user.id}
+                        username={user.username}
+                        image={user.image}
+                        isFollow={user.isFollowing}
+                        id={user.id}
+                    />
                 )
             }
             )}
         </li>
+    )
+}
+
+const ProfileMin = (props) => {
+    const [profile, dispatch] = useReducer(followReducer, initialState)
+    const { state } = useContext(AuthContext)
+    useEffect(() => {
+        if (props.isFollow) dispatch({ type: 'FOLLOW' })
+    }, [])
+
+    return (
+        <ul className={styles.profile__item}>
+            <Link to={`/${props.username}`}>
+                <ProfileImage
+                    className={styles.profile__user__icon}
+                    src={props.image}
+                />
+                <span className={styles.profile__user}>{props.username}</span>
+            </Link>
+            <FollowButton
+                onFollow={followRequest.bind(this, props.id, dispatch)}
+                onUnfollow={unfollowRequest.bind(this, props.id, dispatch)}
+                isFollowing={profile.isFollow}
+                myProfile={props.username === state.user}
+            />
+        </ul>
     )
 }
 
