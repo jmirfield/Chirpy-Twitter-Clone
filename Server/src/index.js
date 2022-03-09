34de4c8389171d9030b5e95ed018da5c0be26a46
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-09 17:29:00 -0500

    Changed port settings

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
