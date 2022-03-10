db6669926acabdae67fad698a9587d13b53d2649
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-10 14:22:45 -0500

    Back to dev

--- Client/src/api/constants.js
+++ Client/src/api/constants.js
@@ -1,3 +1,3 @@
-// export const API_URL = 'http://localhost:3001/';
+export const API_URL = 'http://localhost:3001/';
 // export const API_URL = 'http://192.168.0.7:3001/';
-export const API_URL = 'https://rocky-retreat-75616.herokuapp.com'
+// export const API_URL = 'https://rocky-retreat-75616.herokuapp.com'
