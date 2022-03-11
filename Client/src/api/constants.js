767396c93e7f749321d04ff76069369459fb9741
Justin Mirfield <justin.mirfield@gmail.com>
2022-03-11 11:38:31 -0500

    Added env for url

--- Client/src/api/constants.js
+++ Client/src/api/constants.js
@@ -1,3 +1,1 @@
-// export const API_URL = 'http://localhost:3001/';
-// export const API_URL = 'http://192.168.0.7:3001/';
-export const API_URL = 'https://rocky-retreat-75616.herokuapp.com'
+export const API_URL = process.env.API || 'http://localhost:3001/';
